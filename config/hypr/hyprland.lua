81d3106b4223b2bb647dbfb3d83df2a526d08e73
Paul <paul.robl@pr-d.de>
2026-08-29 14:03:08 +0200

    Hyprland: Matplotlib figures go to special workspace

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -169,6 +169,7 @@ create_custom_workspace("CDS", "T")
 create_custom_workspace("LibraryManager", "Y")
 create_custom_workspace("Console", "BACKSLASH")
 create_custom_workspace("X", "X")
+create_custom_workspace("Output", "O")
 
 
 hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
@@ -209,3 +210,11 @@ if (hostname == "PRTP") then
         scale=1
     })
 end
+
+hl.window_rule({
+    name = "OutputWindowRule", 
+    match = {
+        class = "Matplotlib",
+    },
+    workspace = "special:Output silent",
+})
